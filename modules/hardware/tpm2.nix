79ae9d19503e0f2ae3aa80d4e5dec7baf47b8837
Samuel Martins <s@smartins.ch>
2023-06-24 17:02:42 +0000

    Mpv config

--- modules/hardware/tpm2.nix
+++ modules/hardware/tpm2.nix
@@ -2,7 +2,7 @@
 let
   tpm2-update = pkgs.writeShellScriptBin "tpm2-update" ''
     sudo systemd-cryptenroll --wipe-slot=tpm2 ${config.boot.initrd.luks.devices."nixos".device}; 
-    sudo systemd-cryptenroll --tpm2-pcrs=0+2+4 ${config.boot.initrd.luks.devices."nixos".device}
+    sudo systemd-cryptenroll --tpm2-pcrs=0+2+4 --tpm2-device=/dev/tpmrm0 ${config.boot.initrd.luks.devices."nixos".device}
   '';
 in
 {
