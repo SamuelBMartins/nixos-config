94c5eb293afc7d9602490f68b96e4a3a716b9910
Samuel Martins <s@smartins.ch>
2023-06-24 16:36:35 +0000

    test

--- modules/hardware/tpm2.nix
+++ modules/hardware/tpm2.nix
@@ -1,7 +1,8 @@
-{ pkgs, cryptDisk, ... }: 
+{ config, pkgs, cryptDisk, ... }: 
 let
   tpm2-update = pkgs.writeShellScriptBin "tpm2-update" ''
-    sudo systemd-cryptenroll --wipe-slot=tpm2 ${boot.initrd.luks.devices.${cryptDisk}.device}; sudo systemd-cryptenroll --tpm2-pcrs=0+2+4 ${boot.initrd.luks.devices.${cryptDisk}.device}
+    sudo systemd-cryptenroll --wipe-slot=tpm2 ${config.boot.initrd.luks.devices.${cryptDisk}.device}; 
+    sudo systemd-cryptenroll --tpm2-pcrs=0+2+4 ${config.boot.initrd.luks.devices.${cryptDisk}.device}
   '';
 in
 {
