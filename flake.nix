5e32fb0f2edf7875a9721127f62ed93998f733a8
Samuel Martins <s@smartins.ch>
2023-06-24 09:39:42 +0000

    test

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    arkenfox.url = "github:dwarfmaster/arkenfox-nixos";
     sops-nix.url = "github:Mic92/sops-nix";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
