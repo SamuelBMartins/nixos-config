0ca94711a8adcb5774fd5d7ccc681fbcc117db10
Samuel Martins <s@smartins.ch>
2023-06-24 12:15:36 +0000

    test

---
+++ modules/dev.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }: {
+
+  virtualisation.docker = {
+    enable = true;
+
+    storageDriver = if fileSystems."/".fsType == "btrfs" 
+        then "btrfs" else null;
+  };
+
+  users.users.smartins.extraGroups = [ "docker" ];
+}
