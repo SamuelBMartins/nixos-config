48a70493c0b8c50d83709c446137b37288560c1e
Samuel Martins <s@smartins.ch>
2023-06-24 22:38:02 +0000

    add nerd fonts
    test
    
    test

--- modules/dev.nix
+++ modules/dev.nix
@@ -1,5 +1,10 @@
-{ config, username, ... }: {
+{ config, pkgs, username, ... }: {
 
   virtualisation.docker.enable = true;
   users.users.${username}.extraGroups = [ "docker" ];
+
+  fonts.fonts = with pkgs; [
+    (nerdfonts.override { fonts = [ "Monospace" ]; })
+  ];
+  console.font = "Monospace";
 }
