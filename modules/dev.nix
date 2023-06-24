a580a439dda1191c1341bed98cec7d6be4fbc379
Samuel Martins <s@smartins.ch>
2023-06-24 14:30:34 +0000

    test

--- modules/dev.nix
+++ modules/dev.nix
@@ -1,11 +1,5 @@
 { config, username, ... }: {
 
-  virtualisation.docker = {
-    enable = true;
-    
-    # storageDriver = if config.fileSystems."/".fsType == "btrfs" 
-    #     then "btrfs" else null;
-  };
-
+  virtualisation.docker.enable = true;
   users.users.${username}.extraGroups = [ "docker" ];
 }
