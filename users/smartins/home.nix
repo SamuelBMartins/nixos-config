f1309ea2bf4cf516ca3369f2a4cead6943f627bc
Samuel Martins <s@smartins.ch>
2023-06-24 22:25:49 +0000

    Add neovim config

--- users/smartins/home.nix
+++ users/smartins/home.nix
@@ -8,7 +8,7 @@
     enable = true;
     userName = "Samuel Martins";
     userEmail = "s@smartins.ch";
-    signing.key = null;
+    signing.key = null; # GPG decides appropriate key to use based on email
     signing.signByDefault = true;
   };
 
@@ -23,12 +23,41 @@
       ip = "ip -color=auto";
       clip = "xclip -sel c <";
       lg = "lazygit";
-      vim = "nvim";
     };
   };
 
-  home.sessionVariables = {
-    EDITOR = "nvim";
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    vimAlias = true;
+
+    coc = {
+      enable = true;
+
+      settings = {
+	      "suggest.noselect" = true;
+      };
+    };
+
+    plugins = with pkgs.vimPlugins; [
+      nerdtree
+      lightline-vim
+      vim-devicons
+      vim-startify
+    ];
+
+    extraConfig = ''
+      set relativenumber
+      set ignorecase
+      set tabstop=4
+      set softtabstop=4
+      set expandtab
+      set shiftwidth=4
+      set autoindent
+      set cc=80
+      set mouse=a
+      set cursorline
+    '';
   };
  
   home.stateVersion = "23.05";
