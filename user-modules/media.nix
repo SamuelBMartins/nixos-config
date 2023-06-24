862b24c1ea24fe4800a540e95419a7f4fd71efec
Samuel Martins <s@smartins.ch>
2023-06-24 16:01:49 +0000

    test

--- user-modules/media.nix
+++ user-modules/media.nix
@@ -2,17 +2,7 @@
 
 {
 
-  config.networking.firewall = {
-    allowedTCPPorts = [3000];
-    allowedUDPPorts = [
-      32410
-      32412
-      32413
-      32414
-    ];
-  };
-
-  config.home.packages = with pkgs; [
+  home.packages = with pkgs; [
     plex-media-player
     jellyfin-web
     mpv
