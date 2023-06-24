21b01906e0573b064c56aaa55b65ca6c1b5f972c
Samuel Martins <s@smartins.ch>
2023-06-24 11:44:14 +0000

    Fix steam
    test

--- user-modules/gaming.nix
+++ user-modules/gaming.nix
@@ -1,12 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = false;
-  };
-
   home.packages = with pkgs; [
     discord
     bottles
