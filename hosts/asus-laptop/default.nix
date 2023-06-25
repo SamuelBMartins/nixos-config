4c082209b7c15d6f4c45e9a0b09889459f2863aa
Samuel Martins <s@smartins.ch>
2023-06-25 11:32:24 +0000

    test

--- hosts/asus-laptop/default.nix
+++ hosts/asus-laptop/default.nix
@@ -12,6 +12,11 @@
 
   console.keyMap = "fr_CH";
 
+ services.xserver = {
+    layout = "ch";
+    xkbVariant = "fr";
+  };
+  
   nixpkgs.config.allowUnfree = true;
   
   system.stateVersion = "23.05";
--- users/smartins/modules/graphical/default.nix
+++ users/smartins/modules/graphical/default.nix
@@ -17,9 +17,6 @@ in
     "org/gnome/desktop/background" = {
       picture-uri = "${../../assets/walls/anime-wallpaper1.jpg}";
     };
-    "org/gnome/desktop/input-sources" = {
-      sources = [ (mkTuple [ "xkb" "us+altgr-intl" ]) (mkTuple [ "xkb" "ch+fr" ]) ];
-    };
   };
 
   home.file.".face".source = ../../assets/icon.png;
