03bf1b823a502282c09479299f8bf0d3f03c6d54
Samuel Martins <s@smartins.ch>
2023-06-25 09:06:45 +0000

    test

--- flake.nix
+++ flake.nix
@@ -33,9 +33,7 @@
               ] ++ map (x: ./. + "/users/${systemConfig.username}/modules/" + x) systemConfig.categories;
             };
           }
-        ] 
-        ++ systemConfig.modules
-        ++ map (x: ./. + "/modules/" + x) systemConfig.categories;
+        ] ++ map (x: ./. + "/modules/" + x) systemConfig.categories;
 
         specialArgs = { username = systemConfig.username; };
       };
