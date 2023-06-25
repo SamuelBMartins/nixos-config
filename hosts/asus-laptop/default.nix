673255990eb2e27b53e97bd279909cc1977eb905
Samuel Martins <s@smartins.ch>
2023-06-25 10:08:38 +0000

    layout asus

--- hosts/asus-laptop/default.nix
+++ hosts/asus-laptop/default.nix
@@ -10,12 +10,13 @@
 
   networking.hostName = "asus-laptop";
 
-  services.xserver = {
-    layout = "ch";
-    xkbVariant = "fr";
-  };
-
   console.keyMap = "fr_CH";
+  services.xserver.desktopManager.gnome = { 
+    extraGSettingsOverrides = '' 
+      [org.gnome.desktop.input-sources] 
+      sources=[('xkb', 'fr_CH')]
+    ''; 
+  };
 
   nixpkgs.config.allowUnfree = true;
   
