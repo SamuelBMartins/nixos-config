6af1e7cdf480d8e3ab52ec9596548a7c0084e150
Samuel Martins <s@smartins.ch>
2023-07-05 21:56:32 +0000

    Add config if no wall

--- users/smartins/modules/graphical/gnome.nix
+++ users/smartins/modules/graphical/gnome.nix
@@ -5,25 +5,30 @@ let
 in
 lib.mkIf systemConfig.services.xserver.desktopManager.gnome.enable {
   # TODO name packages auto
-  dconf.settings = {
+  dconf.settings = lib.mkMerge [
+    {
+      "org/gnome/desktop/input-sources" = {
+        sources = [ ] ++ map (x: (mkTuple [ "xkb" x ])) userConfig.keyboardLayout;
+      };
+      "org/gnome/shell" = {
+        enabled-extensions = [ "appindicatorsupport@rgcjonas.gmail.com" ];
+        disabled-extensions = [];
+        favorite-apps = [
+          "librewolf.desktop"
+          "org.gnome.Geary.desktop"
+          "org.gnome.Calendar.desktop"
+          "org.gnome.Nautilus.desktop"
+          "org.gnome.Console.desktop"
+        ];
+      };
+    }
+
+    (lib.mkIf (wall ? userConfig) {
     "org/gnome/desktop/background" = {
-      picture-uri = "${userConfig.wall}";
-    };
-    "org/gnome/desktop/input-sources" = {
-      sources = [ ] ++ map (x: (mkTuple [ "xkb" x ])) userConfig.keyboardLayout;
-    };
-    "org/gnome/shell" = {
-      enabled-extensions = [ "appindicatorsupport@rgcjonas.gmail.com" ];
-      disabled-extensions = [];
-      favorite-apps = [
-        "librewolf.desktop"
-        "org.gnome.Geary.desktop"
-        "org.gnome.Calendar.desktop"
-        "org.gnome.Nautilus.desktop"
-        "org.gnome.Console.desktop"
-      ];
-    };
-  };
+        picture-uri = "${userConfig.wall}";
+      };
+    })
+  ];
 
   home.file.".face".source = ../../assets/icon.png;
 }
